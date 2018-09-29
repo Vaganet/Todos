925b4b64b4742ef0916001c1b8e2f5de6d6edf72
nadia <sanaa.nadiya@yahoo.fr>
2018-09-29 16:48:09 +0200

    api

---
+++ VAGANET/Todo-app/todo-app-api/src/main/java/com/example/demo/entity/Todo.java
@@ -0,0 +1,61 @@
+package com.example.demo.entity;
+
+public class Todo {
+	
+
+	private String title;
+	
+	
+	
+
+	public Todo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Todo(String title) {
+		super();
+		this.title = title;
+	
+	}
+	
+
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+
+
+	
+
+}
---
+++ VAGANET/Todo-app/todo-app-api/src/main/java/com/example/demo/service/TodoMockService.java
@@ -0,0 +1,52 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Todo;
+
+@Service
+public class TodoMockService implements ITodoService{
+
+	private List<Todo> todos;
+
+	
+	public TodoMockService( ){
+		
+		todos = new ArrayList<Todo>();
+		
+		 todos.add(new Todo( "Setting Up The Angular5 front-end Project"));
+		 todos.add(new Todo( "Setting Up The Spring boot back-end Project"));
+		 todos.add(new Todo("Implementing the Add Task feature"));	 
+	}
+	
+	@Override
+	public List<Todo> getTodos() {
+		return todos;
+	}
+	
+	@Override
+	public void addTodo(Todo todo) {
+		todos.add(todo);
+		
+	}
+
+	@Override
+	public void updateTodo(Todo todo) {
+		todos.remove(todo);
+		todos.add(todo);
+	}
+
+
+	@Override
+	public void deleteTodo(String title) {
+	Todo todo =new Todo();
+	todo.setTitle(title);
+	todos.remove(todo);	
+		
+	}
+
+}
